34506df16d57e6387aa1e847e8ccce9c1c024251
Taru Vikström <taru.vikstrom@gmail.com>
2023-01-19 15:24:07 +0200

    nappuloiden piirto käännös

--- shakki/kayttoliittyma.cpp
+++ shakki/kayttoliittyma.cpp
@@ -49,9 +49,9 @@ void Kayttoliittyma::piirraLauta()
 
 
 
-			if (_asema->_lauta[i][j] != NULL) {
+			if (_asema->_lauta[7-i][7-j] != NULL) {
 
-				wcout << " " << _asema->_lauta[i][j]->getUnicode() << " ";
+				wcout << " " << _asema->_lauta[7-i][7-j]->getUnicode() << " ";
 			}
 			else {
 				wcout << "   ";
